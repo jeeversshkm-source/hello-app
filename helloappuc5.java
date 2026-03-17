176ea059106529c4f42ff0153b9d4618212ae114
jeeverssh k m <jeeversshkm@gmail.com>
2026-03-17 09:16:29 +0530

    added helloappuc5

--- helloappuc5.java
+++ helloappuc5.java
@@ -1,12 +1,3 @@
-/**
- * UC 5: Display "Hello" with Multiple Command-Line Arguments using Enhanced For Loop or Default Message
- * The application should accept multiple names as command-line arguments and display a personalized greeting
- * for each user using an enhanced for loop. If no names are provided, it should display "Hello, World!".
- *
- * Usage: java HelloApp [name1] [name2] ... [nameN]
- * - If names are provided, it will display "Hello, [Name1], [Name2], ...!"
- * - If no names are provided, it will display "Hello, World!"
- */
 
 public class helloappuc5 {
     public static void main(String[] args) {
